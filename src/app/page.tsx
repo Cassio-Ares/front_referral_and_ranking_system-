27c95d6e1379eb40c65a9b896a4bc72af4cb3343
Cassio Silva <137903978+Cassio-Ares@users.noreply.github.com>
2025-09-15 07:15:05 +0000

    btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Input } from "@/app/components/Input";
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <Input placeholder="text 1" />
+      <Input placeholder="text 2" error />
     </main>
   );
 }
